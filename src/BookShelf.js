b51d574c52f1b678b1d3266066f9b203a0cec0eb
gao yang <hzgaoyang5@corp.netease.com>
2018-07-16 19:13:02 +0800

    bug fixed

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -20,7 +20,7 @@ class BookShelf extends React.Component{
                                                     <option value="moveTo" disabled>Move to...</option>
                                                     <option value="currentlyReading">Currently Reading</option>
                                                     <option value="wantToRead">Want to Read</option>
-                                                    <option value="read">Read</option>
+                                                    <option value="readed">Read</option>
                                                     <option value="none">None</option>
                                                 </select>
                                             </div>
